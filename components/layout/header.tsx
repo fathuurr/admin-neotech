1997870ba605f2123c7b8fb4045e241c379ba8d9
Fathurrahman <41844599+fathuurr@users.noreply.github.com>
2024-03-15 14:30:54 +0700

    remove unused var

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -3,7 +3,6 @@ import ThemeToggle from "@/components/layout/ThemeToggle/theme-toggle";
 import { cn } from "@/lib/utils";
 
 import { MobileSidebar } from "./mobile-sidebar";
-import UserNav from "./user-nav";
 import Link from "next/link";
 
 import {
