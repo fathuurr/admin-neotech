e96d77fbba779cb316a0eef9fd3effba3cefd9de
Fathurrahman <41844599+fathuurr@users.noreply.github.com>
2024-03-27 19:52:44 +0700

    implement countdown

--- app/(dashboard)/dashboard/product/[id]/page.tsx
+++ app/(dashboard)/dashboard/product/[id]/page.tsx
@@ -5,13 +5,17 @@ import ModalUploadPhoto from '@/components/modal/product/ModalUploadPhoto';
 import { Heading } from '@/components/ui/heading';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { toast } from '@/components/ui/use-toast';
+import { AlertModal } from '@/components/modal/alert-modal';
+import { Trash2 } from 'lucide-react';
+
 import { deletePhotoProduct, getProductById } from '@/service/product';
 import { Product } from '@/types/product';
-import { Trash2 } from 'lucide-react';
 import { useEffect, useState } from 'react';
 
 const ProductById = ({ params }: { params: { id: string } }) => {
   const [productDetail, setProductDetail] = useState<Product | null>(null);
+  const [deleteImage, setDeleteImage] = useState<string>(''); // State to store the image to be deleted
+  const [showConfirmation, setShowConfirmation] = useState<boolean>(false); // State to control modal visibility
 
   const breadcrumbItems = [
     { title: 'Product', link: '/dashboard/product' },
@@ -30,8 +34,21 @@ const ProductById = ({ params }: { params: { id: string } }) => {
 
   const deletePhoto = async (path: string) => {
     try {
+      // Set the image path to be deleted
+      setDeleteImage(path);
+      // Show confirmation modal
+      setShowConfirmation(true);
+    } catch (error: any) {
+      toast({
+        title: error.message,
+      });
+    }
+  };
+
+  const handleDeleteConfirmation = async () => {
+    try {
       const requestBody = {
-        path: path,
+        path: deleteImage,
       };
 
       const res = await deletePhotoProduct(
@@ -53,6 +70,9 @@ const ProductById = ({ params }: { params: { id: string } }) => {
       toast({
         title: error.message,
       });
+    } finally {
+      // Close confirmation modal after deletion
+      setShowConfirmation(false);
     }
   };
 
@@ -62,59 +82,67 @@ const ProductById = ({ params }: { params: { id: string } }) => {
   }, []);
 
   return (
-    <ScrollArea className='h-full'>
-      <div className='flex-1 space-y-4 p-4 md:p-8 pt-6'>
-        <BreadCrumb items={breadcrumbItems} />
-        <Heading
-          title={productDetail?.productName}
-          description={`Category: ${productDetail?.productCategory.categoryName}`}
-        />
+    <>
+      <AlertModal
+        isOpen={showConfirmation}
+        onClose={() => setShowConfirmation(false)}
+        onConfirm={handleDeleteConfirmation}
+      />
 
-        <div className='mt-5'>
-          <span className='font-semibold'>Description :</span>
-          <p>{productDetail?.productDescription}</p>
-
-          <div className='mt-5'>
-            <span className='font-semibold'>Warranty :</span>
-            <p>{productDetail?.productWarranty} Months </p>
-          </div>
+      <ScrollArea className='h-full'>
+        <div className='flex-1 space-y-4 p-4 md:p-8 pt-6'>
+          <BreadCrumb items={breadcrumbItems} />
+          <Heading
+            title={productDetail?.productName}
+            description={`Category: ${productDetail?.productCategory.categoryName}`}
+          />
 
           <div className='mt-5'>
-            <span className='font-semibold'>Serial Number :</span>
-            <p>{productDetail?.serialNumber.join(', ')}</p>
-          </div>
+            <span className='font-semibold'>Description :</span>
+            <p>{productDetail?.productDescription}</p>
 
-          <div className='mt-5'>
-            <span className='font-semibold mb-2'>Product Image :</span>
-            <div className='mt-2 grid grid-cols-2 md:grid-cols-5 gap-10'>
-              {Array.isArray(productDetail?.productImage) &&
-                productDetail?.productImage.map(
-                  (imageUrl: string, index: number) => (
-                    <div key={index} className='relative'>
-                      <img
-                        src={`${process.env.NEXT_PUBLIC_API_URL}/${imageUrl}`}
-                        className='h-auto max-w-full rounded-lg'
-                        alt='Product Image'
-                        loading='lazy'
-                      />
-
-                      <Trash2
-                        onClick={() => {
-                          deletePhoto(imageUrl);
-                        }}
-                        className='text-red-500 mt-5 cursor-pointer '
-                        size={20}
-                      />
-                    </div>
-                  )
-                )}
+            <div className='mt-5'>
+              <span className='font-semibold'>Warranty :</span>
+              <p>{productDetail?.productWarranty} Months </p>
             </div>
-          </div>
 
-          <ModalUploadPhoto product={params.id} />
+            <div className='mt-5'>
+              <span className='font-semibold'>Serial Number :</span>
+              <p>{productDetail?.serialNumber.join(', ')}</p>
+            </div>
+
+            <div className='mt-5'>
+              <span className='font-semibold mb-2'>Product Image :</span>
+              <div className='mt-2 grid grid-cols-2 md:grid-cols-5 gap-10'>
+                {Array.isArray(productDetail?.productImage) &&
+                  productDetail?.productImage.map(
+                    (imageUrl: string, index: number) => (
+                      <div key={index} className='relative'>
+                        <img
+                          src={`${process.env.NEXT_PUBLIC_API_URL}/${imageUrl}`}
+                          className='h-auto max-w-full rounded-lg'
+                          alt='Product Image'
+                          loading='lazy'
+                        />
+
+                        <Trash2
+                          onClick={() => {
+                            deletePhoto(imageUrl);
+                          }}
+                          className='text-red-500 mt-5 cursor-pointer '
+                          size={20}
+                        />
+                      </div>
+                    )
+                  )}
+              </div>
+            </div>
+
+            <ModalUploadPhoto product={params.id} />
+          </div>
         </div>
-      </div>
-    </ScrollArea>
+      </ScrollArea>
+    </>
   );
 };
 
--- components/modal/alert-modal.tsx
+++ components/modal/alert-modal.tsx
@@ -1,7 +1,6 @@
-"use client";
-import { useEffect, useState } from "react";
-import { Button } from "@/components/ui/button";
-import { Modal } from "@/components/ui/modal";
+import { useEffect, useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { Modal } from '@/components/ui/modal';
 
 interface AlertModalProps {
   isOpen: boolean;
@@ -17,10 +16,45 @@ export const AlertModal: React.FC<AlertModalProps> = ({
   loading,
 }) => {
   const [isMounted, setIsMounted] = useState(false);
+  const [countdown, setCountdown] = useState(5); // Initial countdown time
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout | null = null;
+
+    if (isOpen && countdown > 0) {
+      timer = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1); // Decrease countdown every second
+      }, 1000);
+    }
+
+    return () => {
+      if (timer) clearInterval(timer); // Clear the interval when component unmounts or modal closes
+    };
+  }, [isOpen, countdown]);
+
+  useEffect(() => {
+    // Reset countdown when modal opens
+    if (isOpen && countdown === 0) {
+      setCountdown(5);
+    }
+  }, [isOpen, countdown]);
+
+  useEffect(() => {
+    if (countdown === 0) {
+      onClose(); // Close the modal when countdown reaches 0
+    }
+  }, [countdown, onClose]);
+
+  const handleCancel = () => {
+    onClose(); // Close the modal when cancel button is clicked
+    setCountdown(5); // Reset countdown to initial value when cancel button is clicked
+  };
+
+  const cancelButtonText = countdown > 0 ? `Cancel (${countdown}s)` : 'Cancel';
 
   if (!isMounted) {
     return null;
@@ -28,16 +62,15 @@ export const AlertModal: React.FC<AlertModalProps> = ({
 
   return (
     <Modal
-      title="Are you sure?"
-      description="This action cannot be undone."
+      title='Are you sure?'
+      description='This action cannot be undone.'
       isOpen={isOpen}
-      onClose={onClose}
-    >
-      <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-        <Button disabled={loading} variant="outline" onClick={onClose}>
-          Cancel
+      onClose={onClose}>
+      <div className='pt-6 space-x-2 flex items-center justify-end w-full'>
+        <Button disabled={loading} variant='outline' onClick={handleCancel}>
+          {cancelButtonText}
         </Button>
-        <Button disabled={loading} variant="destructive" onClick={onConfirm}>
+        <Button disabled={loading} variant='destructive' onClick={onConfirm}>
           Continue
         </Button>
       </div>
