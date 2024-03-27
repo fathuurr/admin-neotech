10b1490dd12007b9bb5aa656b2cf1008cf75c251
Fathurrahman <41844599+fathuurr@users.noreply.github.com>
2024-03-27 18:02:19 +0700

    nested breadcrumb

--- components/breadcrumb.tsx
+++ components/breadcrumb.tsx
@@ -1,7 +1,7 @@
-import { cn } from "@/lib/utils";
-import { ChevronRightIcon } from "@radix-ui/react-icons";
-import Link from "next/link";
-import React from "react";
+import { cn } from '@/lib/utils';
+import { ChevronRightIcon } from '@radix-ui/react-icons';
+import Link from 'next/link';
+import React from 'react';
 
 type BreadCrumbType = {
   title: string;
@@ -14,27 +14,24 @@ type BreadCrumbPropsType = {
 
 export default function BreadCrumb({ items }: BreadCrumbPropsType) {
   return (
-    <div className="mb-4 flex items-center space-x-1 text-sm text-muted-foreground">
+    <div className='mb-4 flex items-center space-x-1 text-sm text-muted-foreground'>
       <Link
-        href={"/dashboard"}
-        className="overflow-hidden text-ellipsis whitespace-nowrap"
-      >
+        href={'/dashboard'}
+        className='overflow-hidden text-ellipsis whitespace-nowrap'>
         Dashboard
       </Link>
       {items?.map((item: BreadCrumbType, index: number) => (
         <React.Fragment key={item.title}>
-          <ChevronRightIcon className="h-4 w-4" />
-          <Link
-            href={item.link}
-            className={cn(
-              "font-medium",
-              index === items.length - 1
-                ? "text-foreground pointer-events-none"
-                : "text-muted-foreground",
-            )}
-          >
-            {item.title}
-          </Link>
+          <ChevronRightIcon className='h-4 w-4' />
+          {index === items.length - 1 ? (
+            <span className='font-medium text-foreground'>{item.title}</span>
+          ) : (
+            <Link
+              href={item.link}
+              className='font-medium text-muted-foreground'>
+              {item.title}
+            </Link>
+          )}
         </React.Fragment>
       ))}
     </div>
