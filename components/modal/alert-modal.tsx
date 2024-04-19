3477e3590baf4b96964639a53c156504448ee22f
Fathurrahman <41844599+fathuurr@users.noreply.github.com>
2024-04-19 09:19:22 +0700

    fix alert modal

--- components/modal/alert-modal.tsx
+++ components/modal/alert-modal.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useEffect, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Modal } from '@/components/ui/modal';
@@ -6,7 +7,7 @@ interface AlertModalProps {
   isOpen: boolean;
   onClose: () => void;
   onConfirm: () => void;
-  loading?: boolean;
+  loading: boolean;
 }
 
 export const AlertModal: React.FC<AlertModalProps> = ({
@@ -16,45 +17,10 @@ export const AlertModal: React.FC<AlertModalProps> = ({
   loading,
 }) => {
   const [isMounted, setIsMounted] = useState(false);
-  const [countdown, setCountdown] = useState(5); // Initial countdown time
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
-
-  useEffect(() => {
-    let timer: NodeJS.Timeout | null = null;
-
-    if (isOpen && countdown > 0) {
-      timer = setInterval(() => {
-        setCountdown((prevCountdown) => prevCountdown - 1); // Decrease countdown every second
-      }, 1000);
-    }
-
-    return () => {
-      if (timer) clearInterval(timer); // Clear the interval when component unmounts or modal closes
-    };
-  }, [isOpen, countdown]);
-
-  useEffect(() => {
-    // Reset countdown when modal opens
-    if (isOpen && countdown === 0) {
-      setCountdown(5);
-    }
-  }, [isOpen, countdown]);
-
-  useEffect(() => {
-    if (countdown === 0) {
-      onClose(); // Close the modal when countdown reaches 0
-    }
-  }, [countdown, onClose]);
-
-  const handleCancel = () => {
-    onClose(); // Close the modal when cancel button is clicked
-    setCountdown(5); // Reset countdown to initial value when cancel button is clicked
-  };
-
-  const cancelButtonText = countdown > 0 ? `Cancel (${countdown}s)` : 'Cancel';
 
   if (!isMounted) {
     return null;
@@ -67,8 +33,8 @@ export const AlertModal: React.FC<AlertModalProps> = ({
       isOpen={isOpen}
       onClose={onClose}>
       <div className='pt-6 space-x-2 flex items-center justify-end w-full'>
-        <Button disabled={loading} variant='outline' onClick={handleCancel}>
-          {cancelButtonText}
+        <Button disabled={loading} variant='outline' onClick={onClose}>
+          Cancel
         </Button>
         <Button disabled={loading} variant='destructive' onClick={onConfirm}>
           Continue
